d28f914e41774b271d60eadf2e1e634eb1ed745b
Eric Boone <ericboone@live.com>
2023-07-22 22:08:44 -0500

    Add files via upload

--- finfinbot.py
+++ finfinbot.py
@@ -8,18 +8,18 @@ with open("token") as f:
 async def on_ready():
     print(f"We have logged in as {bot.user}")
 
-@bot.slash_command(description="Say Hello to FinFin!", guild_ids=[])
+@bot.slash_command(description="Say Hello to FinFin!", guild_ids=[877716787305799740])
 async def hello(ctx):
     await ctx.respond("Hello!")
 
-@bot.slash_command(description="Get bot latency", guild_ids=[])
+@bot.slash_command(description="Get bot latency", guild_ids=[877716787305799740])
 async def ping(ctx):
-     await ctx.send(f'Pong! In {round(bot.latency * 1000)}ms')
+     await ctx.respond(f'Pong! In {round(bot.latency * 1000)}ms')
 
-@bot.slash_command(description="Send Image", guild_ids=[])
+@bot.slash_command(description="Send Image", guild_ids=[877716787305799740])
 async def showmefinfin(ctx):
      with open('finfin1.jpg', 'rb') as f:
         picture = discord.File(f)
-        await ctx.send(file=picture)
+        await ctx.respond(file=picture)
 
 bot.run(bottoken)
