4408fb533cf835dc0e77ef3ea508bc648fda0af4
Eric Boone <ericboone@live.com>
2023-07-22 21:54:17 -0500

    Update finfinbot.py

--- finfinbot.py
+++ finfinbot.py
@@ -8,15 +8,15 @@ with open("token") as f:
 async def on_ready():
     print(f"We have logged in as {bot.user}")
 
-@bot.slash_command(description="Say Hello to FinFin!", guild_ids=[877716787305799740])
+@bot.slash_command(description="Say Hello to FinFin!", guild_ids=[])
 async def hello(ctx):
     await ctx.respond("Hello!")
 
-@bot.slash_command(description="Get bot latency", guild_ids=[877716787305799740])
+@bot.slash_command(description="Get bot latency", guild_ids=[])
 async def ping(ctx):
      await ctx.send(f'Pong! In {round(bot.latency * 1000)}ms')
 
-@bot.slash_command(description="Send Image", guild_ids=[877716787305799740])
+@bot.slash_command(description="Send Image", guild_ids=[])
 async def showmefinfin(ctx):
      with open('finfin1.jpg', 'rb') as f:
         picture = discord.File(f)
